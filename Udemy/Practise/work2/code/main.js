96327d8a57aa00f3929638f47049119f0243d35f
watersmark <watersmark@mail.ru>
2020-12-01 15:18:38 +0300

    add chapter 20 p2

--- Udemy/Practise/work2/code/main.js
+++ Udemy/Practise/work2/code/main.js
@@ -1,36 +1,38 @@
 `use strict`;
 
 // создаём объект
+// внутри объекта создадим свойства
+// так же создадим методы
 const options = {
     name: 'Sanya',
     width: 1024,
     height: 1024,
+    
     color: {
         border: 'black',
         bg: 'red'
+    },
+
+    makeTest: function(firsttArg, secondArg){
+        return firsttArg + secondArg;
     }
+
 };
 
-// для удаления элемента из объекта
-// используем оператор delete
-// свойство будет полностью удалено
-delete options.name;
-console.log(options);
 
-// для перебора всех свойств объектов
-// используем цикл
-// внутри цикла проверяем, что является ли наше 
-// свойство вложенным объектом
-for (let key in options){
-    if(typeof(options[key]) === 'object'){
-        for(let i in options[key]){
-            console.log(`войство ${i} имеет значение ${options[key][i]}`); 
-        }
-    }
-    else{
-        console.log(`Свойство ${key} имеет значение ${options[key]}`);
-    }
+// мы можем получить массив ключей из объекта
+// исользуем interface Object и его метод keys
+// потом берём у массива метод length и получаем
+// кол-во свойств в объекте
+console.log(Object.keys(options).length);
 
-   
-}
+// вызовем метод из объекта
+console.log(options.makeTest(1, 41));
 
+
+// для деструктуризации, можно вытаскивать объекты 
+// из родительского объекта
+
+const {border, bg} = options.color;
+console.log(border);
+console.log(bg);
