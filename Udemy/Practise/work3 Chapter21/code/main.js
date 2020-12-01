b164b639419662b0e48774b22529ecb0926d8c81
watersmark <watersmark@mail.ru>
2020-12-01 19:58:20 +0300

    Add chapter 22 part 2

--- Udemy/Practise/work3 Chapter21/code/main.js
+++ Udemy/Practise/work3 Chapter21/code/main.js
@@ -1,28 +1,9 @@
 `use strict`;
- 
-// при передаче примитивов в переменную записывается 
-// байт код назначенного значения
-// объекты передаются по ссылке
 
-
-// чтобы скопировать объект можно использовать цикл
-// данный метод может скопировать отдельные переменные
-// но если у нас есть объект в объекте, то метод
-// не сделает отдельную копию, а кинет уже существующую ссылку
-function copy(mainObj){
-    let objCopy = {};
-
-    let key;
-    for (key in mainObj){
-
-        objCopy[key] = mainObj[key];
-
-    }
-
-    return objCopy;
-}
-
-
+// чтобы соеденить несколько объектов в один
+// используем метод интерфейса Object
+// данный метод можно использовать для поверхностного клонирования 
+// объектов
 const numbers = {
     a: 2,
     b: 5,
@@ -32,10 +13,57 @@ const numbers = {
     }
 };
 
+const add = {
+    d: 17,
+    e: 20
+};
 
-const numbersNew = copy(numbers);
+// переносим одтн объект в другой
+const tempConcat = Object.assign(numbers, add)
 
-numbers.a = 323223;
+// клонируем поверхностно в пустой объект
+// уже существующий массив add
+const clone = Object.assign({}, add);
 
-console.log(numbers);
-console.log(numbersNew);
+
+// При клонировании массивов
+// можно использовать slice
+// Получим два независимых массива
+const arrayOld = [1, 3, 4];
+const arrayNew = arrayOld.slice();
+
+
+// чтобы переместить несколько массивов в один
+// используем синтаксис преведённый ниже
+// называемый оператором разворота
+const video = ['youtube', 'vimeo', 'facebook'],
+    blogs = ['wordpress', 'leveblog', 'blogger'],
+    internet = [...video, ...blogs, 'vk', 'mail'];
+
+console.log(internet);
+console.log(typeof(internet));
+
+
+// чтобы разложить массив на отдельные 
+// элементы используем (...mass)
+
+let mass = [1, 7, 22];
+
+function log(a, b, c){
+    console.log(a, b, c);
+}
+
+log(...mass);
+
+// данным методом можно тоже создать копию
+// получаем ссылку на новый объект
+const array1 = [...mass];
+console.log(array1);
+
+// для копирования объектов
+const q = {
+    one: 1,
+    two: 2
+};
+const obj = {...q};
+console.log(obj);
