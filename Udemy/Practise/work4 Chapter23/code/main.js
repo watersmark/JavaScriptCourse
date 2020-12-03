ff46a5c987c1bc9faa20771d781ed6f032056814
watersmark <watersmark@mail.ru>
2020-12-03 12:25:32 +0300

    add chapter 26 Cast

--- Udemy/Practise/work4 Chapter23/code/main.js
+++ Udemy/Practise/work4 Chapter23/code/main.js
@@ -1,40 +1,8 @@
 `use strict`;
 
-// создадим два объекта один из которых
-// будет более глобальным чем второй
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function(){
-        console.log("Hello All");
-    }
-};
+let age = 32;
+const name ='Snaya';
 
-// отдельный объект
-const john = {
-      health: 120
-};
-
-// отдельный объект
-
-const mark = {
-    health: 200
-};
-
-// устанавливаем прототип для
-// объекта john, данный метод
-// является устаревшим
-john.__proto__ = soldier;
-console.log(john.sayHello());
-
-// более современный метод создания прототипов
-
-Object.setPrototypeOf(mark, soldier);
-console.log(mark.armor);
-
-// для создания прототипа до инициализации объекта
-
-const maks = Object.create(soldier);
-console.log(maks.sayHello());
-maks.armor =400;
-console.log(maks.armor);
+for(let i =0; i< 32; i++){
+    console.log(i, typeof(i));
+}
