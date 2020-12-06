d98b52ea56a0c9d281d464447316a33c2c7df963
watersmark <watersmark@mail.ru>
2020-12-06 20:29:31 +0300

    add codewars solved problem

--- Udemy/Practise/work5 Chapter26/code/main.js
+++ Udemy/Practise/work5 Chapter26/code/main.js
@@ -1,24 +1,38 @@
 `use strict`;
 
-let stepForResult = 0;
-let lastDigit = null;
-
-function seven(m) {
-
-    let temp1 = parseInt(m / 10);
-    let temp2 = m % 10;
-    let resTempDigit = temp1 - 2 * temp2;
-    
-    if(new String(resTempDigit).length <= 2){
-        lastDigit = resTempDigit;
-        stepForResult++;
-    }
-    else{
-        stepForResult++;
-        seven(resTempDigit);
-    }
-    
-    return [lastDigit, stepForResult];
+// function collable sort
+function sorts(a, b){
+    return a - b;
 }
 
-console.log(seven(1021));
+// main function
+function findOdd(A) {
+
+    // sort mass
+    A.sort(sorts);
+    // count other digit
+    let countDigit = 0;
+    
+    for (let i = 0; i < A.length; i++){
+        
+        // last digit case
+        if(i == A.length - 1){
+            return A[i];
+        }
+
+        // common elem case
+        if(A[i] !== A[i - 1]){
+            if(countDigit % 2 != 0) return A[i - 1];
+            countDigit = 0;
+        }
+
+        // incr
+        countDigit++;
+    }
+
+}
+
+console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]));
+
+
+
