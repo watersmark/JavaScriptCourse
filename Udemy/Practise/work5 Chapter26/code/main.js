c5a27bc2605081cb0aad7e7db72d51f894c9e049
watersmark <watersmark@mail.ru>
2020-12-06 16:49:52 +0300

    add codewars problem

--- Udemy/Practise/work5 Chapter26/code/main.js
+++ Udemy/Practise/work5 Chapter26/code/main.js
@@ -1,22 +1,31 @@
 `use strict`;
 
+function longest(s1, s2) {
+    let map = new Map([["a", 0], ["b", 0], ["c", 0], ["d", 0], ["e", 0], ["f", 0], ["g", 0], ["h", 0], ["i", 0], ["j", 0], ["k", 0], ["l", 0], ["m", 0], ["n", 0],
+                        ["o", 0], ["p", 0], ["q", 0], ["r", 0], ["s", 0], ["t", 0], ["u", 0], ["v", 0], ["w", 0], ["x", 0], ["y", 0], ["z", 0]]);
+    
+    for(let indexElem = 0; indexElem < s1.length; indexElem++){
+        map.set(s1[indexElem], 1);
+    }
 
-// множество можно создать с помощью преобразования 
-// из объекта в map, с помощью метода который преобразует множество пар 
-// объекта в набор массивов с ключ значением, который
-// потом передаётся в конструктор map
+    for(let indexElem = 0; indexElem < s2.length; indexElem++){
+        map.set(s2[indexElem], 1);
+    }
+    
+    let resStr = new String();
 
-let objTest = {
-    name : 'Kostan',
-    age : 32,
-    money : 'dollar'
-};
+    map.forEach((value, item) => {
+        if(value != 0) resStr += item;
+    });
 
-let mapEat = new Map(Object.entries(objTest));
-console.log(mapEat, typeof(mapEat));
+    return resStr;
+}
 
 
-// теперь преобразуем map в объект обратно
 
-let obj = Object.fromEntries(mapEat.entries());
-console.log(obj.name);
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+
+if(longest(a, b) == "abcdefklmopqwxy"){
+    console.log(true);
+}
