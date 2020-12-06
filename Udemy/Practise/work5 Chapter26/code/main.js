8bbf2490f34887771a26ce558a0451b2b6c05faa
watersmark <watersmark@mail.ru>
2020-12-06 15:30:35 +0300

    add practise Map

--- Udemy/Practise/work5 Chapter26/code/main.js
+++ Udemy/Practise/work5 Chapter26/code/main.js
@@ -1,23 +1,31 @@
 `use strict`;
 
-// обращаемся к элементам страницы
-const box = document.getElementById('box'),
-            btns = document.getElementsByTagName('button'),
-            hearts = document.querySelectorAll('.heart');
-
-// ставим для элемента методы поотдельности
-// btns[1].style.borderRadius = '100%';
-
-// ставим несколько тегов для элемента страницы
-box.style.cssText = 'backgroung-color: blue; width: 500px';
-
-// перебираем элементы внутри псевдоцикла
-for(let i = 0; i < hearts.length; i++){
-    hearts[i].style.backgroundColor = 'blue';
+function longest(s1, s2) {
+    
 }
 
+// create map
+let mapTest = new Map();
 
-for(let elem in hearts){
-    console.log(typeof(elem));
-    console.log(hearts.length);
-}
+// добавляем элементы, если элемент уже существовал
+// он просто перезаписывается
+// можно добавлять строки и числа как ключи 
+mapTest.set('temp', 11);
+mapTest.set('temp', 12);
+mapTest.set(5, 1);
+
+// получаем элементы из map по ключу
+console.log(mapTest.get(5));
+console.log(mapTest.get('temp'));
+
+// узнаём размер map, size это
+// свойство, а не метод
+console.log(mapTest.size);
+
+// очищаем map
+// mapTest.clear();
+// console.log(mapTest);
+
+// узнаём есть ли в множестве 
+// ключ который мы передали в метод
+console.log(mapTest.has(5));
