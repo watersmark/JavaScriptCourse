8c250ebc1379e0a91f04dc663b7049b4d91fef2f
watersmark <watersmark@mail.ru>
2020-12-06 15:57:29 +0300

    add map transform

--- Udemy/Practise/work5 Chapter26/code/main.js
+++ Udemy/Practise/work5 Chapter26/code/main.js
@@ -1,29 +1,22 @@
 `use strict`;
 
-// создадим map и добавим в него значения поумолчанию
-let mapEat = new Map([['milk', 250], [{eat : {orn: 454}}, 400], ['bread', 45]]);
+
+// множество можно создать с помощью преобразования 
+// из объекта в map, с помощью метода который преобразует множество пар 
+// объекта в набор массивов с ключ значением, который
+// потом передаётся в конструктор map
+
+let objTest = {
+    name : 'Kostan',
+    age : 32,
+    money : 'dollar'
+};
+
+let mapEat = new Map(Object.entries(objTest));
+console.log(mapEat, typeof(mapEat));
 
 
-// перебираем значения элементов из map ы цикле
-// map можно перебрать только через of
-// в map перебор происходит в порядке вставки элементов
-for (let elem of mapEat){
-    console.log(elem);
-}
+// теперь преобразуем map в объект обратно
 
-// перебираем map по ключам
-for(let elem of mapEat.keys()){
-    console.log(elem);
-}
-
-// перебираем map по значениям
-for(let elem of mapEat.values()){
-    console.log(elem);
-}
-
-// у map есть встроенный метод forEach
-
-mapEat.forEach((value, key, map) => {
-    console.log(key, value);
-});
-
+let obj = Object.fromEntries(mapEat.entries());
+console.log(obj.name);
